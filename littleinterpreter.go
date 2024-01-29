61c31aa4c823b4a93c570037e0b78843fd5b19f8
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-01-29 13:08:46 -0500

    Reset

--- littleinterpreter.go
+++ littleinterpreter.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
+	"fmt"
 )
 
 func main() {
-	data, err := os.ReadFile(os.Args[1])
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data = append(data, 0x0a)
-	VMIsDebuggerOn = true
-	VMRun(VMCreate(GenerateBytecode(GenerateTokens(data))))
+	fmt.Println("Hello world")
 }
--- littlevmecall.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func VMHandleECALL(vm VMState) VMState {
-	vm.status = VM_STATUS_RUNNING
-	return vm
-}
